ef92cce0c4e4120bbfece8a7abb8877278fdb568
Vivek Routh <vvkrth2@gmail.com>
2016-12-18 02:04:17 +0530

    bulk changes

--- app/models/financial_option.rb
+++ app/models/financial_option.rb
@@ -1,8 +1,8 @@
 class FinancialOption < ActiveRecord::Base
-	validates :strike_price, presence: true, length: { in: 1..10 }
-	validates :stock_price, presence: true, length: { in: 1..10 }
-	validates :time_period, presence: true, length: { in: 1..8 }
-	validates :volatility, presence: true, length: { in: 1..10 }
-	validates :interest_rate, presence: true, length: { in: 1..10 }
-	validates :parity_type, presence: true
+	validates :strike_price, presence: true, length: { in: 1..10 }, :numericality => true, :allow_nil => false
+	validates :stock_price, presence: true, length: { in: 1..10 }, :numericality => true, :allow_nil => false
+	validates :time_period, presence: true, length: { in: 1..8 }, :numericality => true, :allow_nil => false
+	validates :volatility, presence: true, length: { in: 1..10 }, :numericality => true, :allow_nil => false
+	validates :interest_rate, presence: true, length: { in: 1..10 }, :numericality => true, :allow_nil => false
+	validates :parity_type, presence: true, :numericality => true, :allow_nil => false
 end
